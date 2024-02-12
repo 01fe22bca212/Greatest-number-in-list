be0341886bd0b00b17e38c5f2b3d46563166ddbd
Vaishnavi kala <01fe22bca212@kletech.ac.in>
2024-02-12 15:30:05 +0530

    code to find the prime numbers

---
+++ prime number/Prime_nums.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Prime_nums{
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the starting number of the range: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the ending number of the range: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers in the range [" + start + ", " + end + "]:");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+        scanner.close();
+    }
+}
